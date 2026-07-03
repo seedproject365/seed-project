a2830831de0367237489fb64ea115132b4e1d8b3
seedproject365 <juzfeifei@gmail.com>
2026-07-03 14:40:32 +0800

    Day 4 homepage

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#F7F2EA] flex items-center justify-center">
+    <div className="text-center">
+
+<Image
+  src="/seed logo.png"
+  alt="Seed Project Logo"
+  width={300}
+  height={300}
+  className="mx-auto mb-6"
+/>
+<button
+  className="rounded-full bg-[#5C4033] px-6 py-3 text-base text-white shadow-md transition duration-300 hover:scale-105 hover:bg-[#6B4B3E]"
+>
+  🌱 Plant your seed now 🌱
+</button>
+</div>
+    </main>
+  );
+}
